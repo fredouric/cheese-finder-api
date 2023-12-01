3bdf512a412268c538a043aa6af52d16e9f5d37e
Frédéric Thomas <frederic.thomas2@icloud.com>
2023-12-01 14:02:55 +0100

    fix: listening port

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port: string | number = process.env.PORT || 8080; // Use 3000 as a default value if PORT is not defined
-
-await app.listen(port);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
